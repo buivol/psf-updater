74561a9370d013f49baf2ea6a4d7d3a83add7cd9
Alexander Tryakin <dev@psf-group.ru>
2018-07-23 17:37:35 +0300

    добавил composer phar

--- composer-update-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd /var/www/dev/data/new/psf/public_html
-composer install --working-dir=/var/www/dev/data/new/psf/public_html --no-ansi --no-interaction --no-scripts --optimize-autoloader
-composer update  --working-dir=/var/www/dev/data/new/psf/public_html --no-ansi --no-interaction --no-scripts --optimize-autoloader
