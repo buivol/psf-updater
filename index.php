34705f66c102202314259e44c5ea0dc8b8d931fa
Alexander Tryakin <dev@psf-group.ru>
2018-07-23 11:16:49 +0300

    test

--- index.php
+++ index.php
@@ -6,4 +6,14 @@
  * Time: 11:05
  */
 
-echo 'hello';
+
+$fInput = file_get_contents("php://input");
+$json = json_decode($fInput);
+$event = $_SERVER['X-GitHub-Event'];
+
+$date = date('H:i:s_d.m.Y');
+$logName = $date . '_' . $event . '.json';
+
+file_put_contents($logName, $fInput);
+
+echo 'hello ' . $event;
