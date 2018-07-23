e1c18d7fa8c050e33f8cb4f8b030fb2d433d8aeb
Alexander Tryakin <dev@psf-group.ru>
2018-07-23 16:34:46 +0300

    args

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if ($event == 'push') {
         echo 'Ответ: ' . PHP_EOL;
         var_dump($out);
         echo PHP_EOL;
-        $command = 'cd ' . $config['develop']['path']['public'] . ' && composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader && composer update --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader';
+        $command = 'cd ' . $config['develop']['path']['public'] . ' && composer install --working-dir=' .$config['develop']['path']['public'] .' --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader && composer update --working-dir=' .$config['develop']['path']['public'] .' --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader';
         $out = shell_exec($command);
         echo 'Выполнена команда: '. $command . PHP_EOL;
         echo 'Ответ: ' . PHP_EOL;
