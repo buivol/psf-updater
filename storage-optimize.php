7b77a681db64e82bac36d6b4d98d7a10ae32cc1a
Alexander Tryakin <dev@psf-group.ru>
2018-07-30 12:51:37 +0300

    add optimizer

--- storage-optimize.php
+++ storage-optimize.php
@@ -12,8 +12,42 @@ $branch = isset($_GET['b']) ? $_GET['b'] : 'develop';
 if (!in_array($branch, ['develop', 'master'])) $branch = 'develop';
 
 $result = [
-  'process' => 'storageOptimize',
-  'branch' => $branch,
+    'process' => 'storageOptimize',
+    'branch' => $branch,
 ];
+
+function dirToArray($dir)
+{
+
+    $result = array();
+
+    $cdir = scandir($dir);
+    foreach ($cdir as $key => $value) {
+        if (!in_array($value, array(".", ".."))) {
+            if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
+                $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+            } else {
+                $result[] = $value;
+            }
+        }
+    }
+
+    return $result;
+}
+
+
+use Spatie\ImageOptimizer\OptimizerChainFactory;
+
+$optimizerChain = OptimizerChainFactory::create();
+//storage/app/uploads/public
+
+// optimize storage/app/uploads/public
+
+$publicPath = $config[$branch]['path']['repo'];
+
+$result['publicPath'] = $publicPath;
+
+//$optimizerChain->optimize($pathToImage);
+
 
 echo json_encode($result);
