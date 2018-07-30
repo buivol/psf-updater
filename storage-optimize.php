0a67a89b9209ea3b711f2b7e407e813754b85eb6
Alexander Tryakin <dev@psf-group.ru>
2018-07-30 12:30:58 +0300

    add optimizer

--- storage-optimize.php
+++ storage-optimize.php
@@ -8,5 +8,12 @@ $config = require_once 'config.php';
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$branch = isset($_GET['b']) ? $_GET['b'] : 'develop';
+if (!in_array($branch, ['develop', 'master'])) $branch = 'develop';
 
-die('ok');
+$result = [
+  'process' => 'storageOptimize',
+  'branch' => $branch,
+];
+
+echo json_encode($result);
