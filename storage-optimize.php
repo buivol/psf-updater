31276033dbee53d21a298068a194c2e82daecdec
Alexander Tryakin <dev@psf-group.ru>
2018-07-30 12:25:04 +0300

    add optimizer

---
+++ storage-optimize.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$config = require_once 'config.php';
+
+require_once __DIR__ . 'vendor/autoload.php';
+
+
+die('ok');
